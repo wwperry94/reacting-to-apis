97449837e806ede100b41d885237552e359790fa
wwperry94 <wwperry94@gmail.com>
2020-10-21 17:39:46 -0500

    minor tweaks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,7 +8,7 @@ export default class App extends Component {
         this.state = {
             hasLoaded: false,
             dataArr: [],
-            usePeople: false,
+            useData: false,
         }
     }
     componentDidMount() {
@@ -32,13 +32,13 @@ export default class App extends Component {
     handleMovie = () => {
         let path = "https://ghibliapi.herokuapp.com/films/";
         this.fetchAPI(path);
-        this.setState({ hasLoaded: true, usePeople: false });
+        this.setState({ hasLoaded: true, useData: false });
 
     }
     handlePeople = () => {
         let path = "https://ghibliapi.herokuapp.com/people";
         this.fetchAPI(path);
-        this.setState({ hasLoaded: true, usePeople: true })
+        this.setState({ hasLoaded: true, useData: true })
     }
     render() {
         console.log(this.state.hasLoaded)
@@ -51,9 +51,9 @@ export default class App extends Component {
                 <br />
                 {!this.state.hasLoaded ? null : this.state.dataArr.map(
                     (data, idx) => {
-                        let itm1 = this.state.usePeople ? data.name : data.title;
-                        let itm2 = this.state.usePeople ? data.age : data.director;
-                        let itm3 = this.state.usePeople ? data.gender : data.description;
+                        let itm1 = this.state.useData ? data.name : data.title;
+                        let itm2 = this.state.useData ? data.age : data.director;
+                        let itm3 = this.state.useData ? data.gender : data.description;
 
                         return <div key={idx} className="shadow-lg p-3 mb-5 bg-white rounded">
                             <div className="card">
@@ -64,7 +64,7 @@ export default class App extends Component {
 
                                     <h5 className="card-title">{itm2}</h5>
                                     <p className="card-text">{itm3}</p>
-                                    {!this.state.usePeople ? null : <a href={data.url} target="_blank" rel="noopener noreferrer" className="btn btn-primary">{`View ${data.name}`}</a>}
+                                    {!this.state.useData ? null : <a href={data.url} target="_blank" rel="noopener noreferrer" className="btn btn-primary">{`View ${data.name}`}</a>}
                                 </div>
                             </div>
                         </div>
